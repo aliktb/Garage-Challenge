4dc79fad43892b9b5008795f0a96f6a46aa1e1f0
aliktb <ali.khattab1@pm.me>
2021-10-12 12:11:46 +0100

    created skeleton for garage class

--- GarageChallenge/src/com/qa/garage/Garage.java
+++ GarageChallenge/src/com/qa/garage/Garage.java
@@ -9,4 +9,65 @@ public class Garage {
 
   List<Vehicle> listOfVehicles = new ArrayList<>();
 
+
+
+  // fixes vehicle based on vehicle's bill
+  public void repair() {
+
+  }
+
+
+  // CRUD methods
+
+  // READ ALL
+  public void seeAllVehicles() {
+
+    System.out.println(listOfVehicles);
+
+  }
+
+
+  // READ by ID
+  public void seeVehicleByID() {
+
+
+
+  }
+
+  // CREATE
+  public void addNewVehicle() {
+
+
+
+  }
+
+
+  // UPDATE
+  public void upgradeVehicle() {
+
+
+
+  }
+
+
+  // DELETE by ID
+  public void removeVehicleByID() {
+
+
+
+  }
+
+  // DELETE ALL
+  public void emptyGarage() {
+
+
+
+  }
+
+  // DELETE vehicles of same type
+
+  public void removeVehicleByType() {
+
+  }
+
 }
